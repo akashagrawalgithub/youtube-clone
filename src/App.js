74555071637b292554edf9a64d08538512f75e86
Akashagrawal <akash242018@gmail.com>
2022-09-01 13:56:15 +0530

    Initial commit for My youtube clone app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import {Navbar,Feed,VideoDetails,ChannelDetails,SearchFeed} from './Components';
+function App() {
+  return (
+    <BrowserRouter>
+      <Box sx={{backgroundColor:'#000'}}/>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Feed/>}/>
+        <Route path="/video/:id" element={<VideoDetails/>}/>
+        <Route path="/channel/:id" element={<ChannelDetails/>}/>
+        <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
